ddee2f018c7ee4917359f6ffd09247b19a85c76b
ishara5 <isharauditha5@gmail.com>
2023-07-23 22:03:08 +0530

    iterative mode activated

--- Grade_counter.py
+++ Grade_counter.py
@@ -25,21 +25,6 @@ def zero_list(n):
         list.append(0)
     return list   
 
-# make grade list
-Grades = ["A", "B", "C", "D", "F","I-we","I-re"]
-Auxileries = ["+", "-"]
-Final_grades = []
-for grade in Grades:
-    Final_grades.append(grade)
-    if grade == "D" or grade == "F" or grade == "I-we" or grade == "I-re":
-        continue    
-    Final_grades.append(grade + Auxileries[0])
-    Final_grades.append(grade + Auxileries[1])
-    
-#make dictionary that count the number of grades   
-initiate_count = zero_list(len(Final_grades))
-Final_grade_count = dict(zip(Final_grades, initiate_count))
-
 file_path = "E:\Programming\Python"
 
 #enter file path
@@ -51,43 +36,71 @@ else:
         file_path = str(input("Enter the Directory path: "))
     else:
         print("Directory path is set to default: ", file_path)
-    
-text_files = get_file_name(file_path)
+        
+initial_file_path = file_path
 
-for i in range(len(text_files)):
-    print(i+1, ":", text_files[i])
-    
-print("input number to select the file: ")
-file_num = int(input())
+# make grade list
+Grades = ["A", "B", "C", "D", "F","I-we","I-re"]
+Auxileries = ["+", "-"]
+Final_grades = []
+for grade in Grades:
+    Final_grades.append(grade)
+    if grade == "D" or grade == "F" or grade == "I-we" or grade == "I-re":
+        continue    
+    Final_grades.append(grade + Auxileries[0])
+    Final_grades.append(grade + Auxileries[1])
 
-result_sheet_name = str(text_files[file_num-1])
-file_path = file_path + "\\" + result_sheet_name
+# iterate until user decide to stop 
+while True:   
+    #make dictionary that count the number of grades   
+    initiate_count = zero_list(len(Final_grades))
+    Final_grade_count = dict(zip(Final_grades, initiate_count))
 
-try:
-    reult_sheet = open(file_path, "r")
-except FileNotFoundError:
-    print("File not found")
-    exit()
+    # get file names
+    text_files = get_file_name(file_path)
 
-# read line by line
-for line in reult_sheet:
-    line = line.strip()
-    line_list = line.split()
+    for i in range(len(text_files)):
+        print(i+1, ":", text_files[i])
+        
+    print("input number to select the file: ")
+    file_num = int(input())
 
-    j = len(line_list)
-    i = 0
-    
-    while i < j:
-        if is_uppercase_final_letter(line_list[i]) and line_list[i+1] in Final_grades:
-            try:
-                Final_grade_count[line_list[i+1]] += 1
-            except KeyError:
-                print("Error result ID: ", line_list[i])
-                exit()
-        # let's check the next id
-        i += 2
+    result_sheet_name = str(text_files[file_num-1])
+    file_path = file_path + "\\" + result_sheet_name
 
-print("For module ", result_sheet_name, " the result is: ")        
-for key in Final_grade_count:      
-    print(key, ":", Final_grade_count[key])
+    try:
+        reult_sheet = open(file_path, "r")
+    except FileNotFoundError:
+        print("File not found")
+        exit()
+
+    # read line by line
+    for line in reult_sheet:
+        line = line.strip()
+        line_list = line.split()
+
+        j = len(line_list)
+        i = 0
+        
+        while i < j:
+            if is_uppercase_final_letter(line_list[i]) and line_list[i+1] in Final_grades:
+                try:
+                    Final_grade_count[line_list[i+1]] += 1
+                except KeyError:
+                    print("Error result ID: ", line_list[i])
+                    exit()
+            # let's check the next id
+            i += 2
+
+    print("For module ", result_sheet_name, " the result is: ")        
+    for key in Final_grade_count:      
+        print(key, ":", Final_grade_count[key])
+        
+    choice = input("Do you want to continue? (y/n): ")
+    if choice == "y":
+        file_path = initial_file_path
+        continue
+    else:
+        print("Thank you for using this program")
+        break
  
